d418802b7d16297389b583a4a6da57fc0529177b
uchenna <uchennaongit@gmail.com>
2021-12-13 18:09:51 +0100

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            &copy; 2021
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
